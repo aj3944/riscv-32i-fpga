640d01cc76619596a583609158761f565bfff808
pmarkowski8 <pm3004@nyu.edu>
2023-11-03 13:25:50 -0400

    Created copy to support older version of Vivado, added design source for data memory

---
+++ copy_of_project/copy_of_project.srcs/sim_1/new/riscv321rv6463_tb.v
@@ -0,0 +1,54 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/31/2023 08:51:42 PM
+// Design Name: 
+// Module Name: riscv321rv6463_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module riscv321rv6463_tb();
+
+    reg t_rst,t_en,t_clk;
+    wire [31:0] t_led_out;
+    
+
+    ahd_6463_risc_v my_processor(
+    .rst(t_rst),
+    .en(t_en),
+    .clk(t_clk),
+    .led_out(t_led_out)
+    );
+    
+    always begin
+        #50 t_clk = ~t_clk;
+    end
+    
+    initial begin
+       t_clk <= 0;
+       t_rst <= 1;
+       t_en <= 1;
+       #100;
+       t_rst <= 0;
+       #10_000_000
+       $display("RISC-V RV32I AHD-6463 WORKING NICELY");
+       $finish;
+    end
+    
+    
+    
+
+endmodule
