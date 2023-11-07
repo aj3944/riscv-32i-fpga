9dd9cf037d11c2e70a4687a9bdfcf04b4935d771
pmarkowski8 <pm3004@nyu.edu>
2023-11-07 09:26:13 -0500

    Modified memory source files and wrote testbenches for memory

--- copy_of_project/copy_of_project.srcs/sim_1/new/mem_data.v
+++ copy_of_project/copy_of_project.srcs/sim_1/new/mem_data.v
@@ -1,23 +1,32 @@
 `timescale 1ns / 1ps
 
+//Note 1: The current memory module is in 'no change' mode where the
+//output does not change when we = 1.
+
+//Note 2: The module already has a register at the output, so DO NOT 
+//add an intermediate register immediately after. This was done to 
+//optimize synthesis according to AMD's documentation guide.
+
 module mem_data #(parameter ADDR_WIDTH = 13, DATA_WIDTH = 32)(
 input [ADDR_WIDTH-1:0] addr,
 input [DATA_WIDTH-1:0] dataIn,
 input clk,
 input we,
-output [DATA_WIDTH-1:0] dout
+output reg [DATA_WIDTH-1:0] dout
 );
 
-reg [DATA_WIDTH-1:0] memory_cell [0:ADDR_WIDTH-1];
+reg [DATA_WIDTH-1:0] memory [0: (2**ADDR_WIDTH)-1];
 
 //Current State Logic
 always@(posedge clk)
 begin
     if(we)
-        memory_cell[addr] <= dataIn;
+    begin
+        //dout <= dataIn;
+        memory[addr] <= dataIn;
+    end
+    else
+        dout <= memory[addr];
 end
-
-//Output Logic
-assign dout = memory_cell[addr];
 
 endmodule
---
+++ copy_of_project/copy_of_project.srcs/sim_1/new/tb_mem_data.v
@@ -0,0 +1,59 @@
+`timescale 1ns / 1ps
+
+module tb_mem_data(
+    );
+    
+    parameter ADDR_WIDTH = 13;
+    parameter DATA_WIDTH = 32;
+    
+    reg clk, we;
+    reg [ADDR_WIDTH-1:0] addr;
+    reg [DATA_WIDTH-1:0] dataIn;
+    wire [DATA_WIDTH-1:0] dout;
+    
+    mem_data #(.ADDR_WIDTH(ADDR_WIDTH), .DATA_WIDTH(DATA_WIDTH)) dut (.clk(clk), .we(we), 
+    .addr(addr), .dataIn(dataIn), .dout(dout));
+    
+    always
+    begin
+        clk <= 1'b0;
+        #5;
+        clk <= 1'b1;
+        #5;
+    end
+    
+    initial
+    begin
+        #120 $finish;
+    end
+    
+    initial
+    begin
+        #10;
+        we = 1'b1;
+        addr = 13'h0000;
+        dataIn = 32'h12345678;
+        #10;
+        addr = 13'h0001;
+        dataIn = 32'h87654321;
+        #10;
+        addr = 13'h0002;
+        dataIn = 32'h01010101;
+        #10;
+        we = 1'b0;
+        addr = 13'h0000;
+        #10;
+        addr = 13'h0001;
+        #10;
+        addr = 13'h0002;
+        #10;
+        we = 1'b1;
+        dataIn = 32'hFFFFFFFF;
+        #10;
+        dataIn = 32'hDDDDDDDD;
+        #10;
+        we = 1'b0;
+        #10;
+    end
+    
+endmodule
