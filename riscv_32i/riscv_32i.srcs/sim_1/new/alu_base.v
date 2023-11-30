857011ea8d299588f13caa0e196d755237b9a66f
aj3944 <aj3944@nyu.edu>
2023-11-30 17:47:58 -0500

    pre milestone 2

--- riscv_32i/riscv_32i.srcs/sim_1/new/alu_base.v
+++ riscv_32i/riscv_32i.srcs/sim_1/new/alu_base.v
@@ -21,6 +21,22 @@
 
 
 module alu_base(
-
+    input wire en,clk,
+    input wire [3:0] control,
+    input wire [31:0] in_buf_1,
+    input wire [31:0] in_buf_2,
+    output wire [31:0] out_buf_1
     );
+        
+    assign out_buf_1 = ( control == 4'b0000) ?  in_buf_1 + in_buf_2:
+            ( control == 4'b0001) ? in_buf_1 << in_buf_2:
+            ( control == 4'b0010) ?  $signed(in_buf_1)   <   $signed(in_buf_2) ?  1 : 0:
+            ( control == 4'b0011) ?  $unsigned(in_buf_1) < $unsigned(in_buf_2) ?  1 : 0: 
+            ( control == 4'b0100) ?  in_buf_1 ^ in_buf_2:
+            ( control == 4'b0101) ?  in_buf_1 >> in_buf_2:
+            ( control == 4'b0110) ?  in_buf_1 | in_buf_2:
+            ( control == 4'b0111) ?  in_buf_1 & in_buf_2:
+            ( control == 4'b1000) ?  in_buf_1 - in_buf_2:
+            $signed(in_buf_1) >> in_buf_2;
 endmodule
+                  
