5dae06660b20cec62f87d23855884f70a6c857c2
aj3944 <aj3944@nyu.edu>
2023-12-14 01:54:54 -0500

    stuck in execution loop

--- c lang codes/main.c
+++ c lang codes/main.c
@@ -1,15 +1,24 @@
+#include <stdbool.h>
 
 int fib(int a){
 	if((a <= 1))
 		return 1;
 	else return fib(a - 2) + fib(a-1);
 }
+
+int noop(int k){
+	if(k) return true;
+	else return false;
+}
 int main(){
 	int a = 30;	
 	// int x = 66666666;
 	int c = fib(a);
+
 	volatile register int p __asm__("x7") = a;
 	volatile register int q __asm__("x8") = 0;
+	noop(q);
 	q = c;
+	noop(q);
 	return c;
 }
