9dcc1f3630da32345afa4e527aad22886a9ecbf6
aj3944 <aj3944@nyu.edu>
2023-12-14 02:24:09 -0500

    working registers

--- c lang codes/main.c
+++ c lang codes/main.c
@@ -1,24 +1,20 @@
-#include <stdbool.h>
 
-int fib(int a){
-	if((a <= 1))
-		return 1;
-	else return fib(a - 2) + fib(a-1);
-}
 
-int noop(int k){
-	if(k) return true;
-	else return false;
-}
+
+
 int main(){
 	int a = 30;	
-	// int x = 66666666;
-	int c = fib(a);
-
-	volatile register int p __asm__("x7") = a;
-	volatile register int q __asm__("x8") = 0;
-	noop(q);
-	q = c;
-	noop(q);
-	return c;
+	int b = 55;	
+	// volatile register int p __asm__("x12") = a;
+	// volatile register int q1 __asm__("x13") = b;
+	// volatile register int q2 __asm__("x14") = b;
+	// volatile register int q3 __asm__("x15") = -4;
+	// volatile register int q4 __asm__("x16") = 2048;
+	// volatile register int q5 __asm__("x17") = 512;
+	// volatile register int q6 __asm__("x18") = 122;
+	// volatile register int q7 __asm__("x19") = 1024;
+	// volatile register int x3 __asm__("x16") = q1 + q3 + q4;
+	// volatile register int x4 __asm__("x17") = q2 + q6 + q7;
+	volatile register int x5 __asm__("x18") = q1 + p + q5;
+	return 0;
 }
